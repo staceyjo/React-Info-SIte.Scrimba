ffcb1830f29fdf3a6d8b5bcb2b048fb6f3d6a217
Stacey <stacey.l.joseph@gmail.com>
2024-05-28 14:50:01 -0400

    updates to local to run

--- index.jsx
+++ index.jsx
@@ -1,15 +1,94 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const navbar = (
-  <nav>
-    <h1>A list that will work eventually</h1>
-    <ul>
-      <li>Hopes</li>
-      <li>Goals</li>
-      <li>Shattered Dreams</li>
-    </ul>
-  </nav>
-);
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-ReactDOM.render(navbar, document.getElementById("root"));
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Dropdown
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline my-2 my-lg-0">
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button
+            className="btn btn-outline-success my-2 my-sm-0"
+            type="submit"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+}
+
+function MainContent() {
+  return <h1>I'm learning React!</h1>;
+}
+
+ReactDOM.render(
+  <div>
+    <Navbar />
+    <MainContent />
+  </div>,
+  document.getElementById("root")
+);
