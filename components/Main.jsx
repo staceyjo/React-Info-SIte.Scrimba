c28d9234d75fb96cc92bde1024996d0f389a04e7
Stacey <stacey.l.joseph@gmail.com>
2024-05-29 19:19:24 -0400

    styling updates to main

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,16 +2,15 @@ import React from "react";
 
 export default function Main() {
   return (
-    <div className="main-content">
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>
-          It's a popular library, so I'll be able to fit in with the cool kids!
-        </li>
-        <li>I'm more likely to get a job as a developer if I know React</li>
+    <div className="main">
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--facts">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ol>
+      </ul>
     </div>
   );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,12 @@ export default function Navbar() {
   return (
     <header>
       <nav>
-        <img src="components/images/react-icon-small.png" />
-        <h3>React Facts</h3>
-        <h4>React Course - Project 1</h4>
+        <img
+          src="components/images/react-icon-small.png"
+          className="nav--icon"
+        />
+        <h3 className="nav--logo_text">React Facts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
         <ul className="nav-items">
           <li>Pricing</li>
           <li>About</li>
