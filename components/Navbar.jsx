04973e2759ebcfc0dbd90a79e356dfb1f8d14ac3
Stacey <stacey.l.joseph@gmail.com>
2024-05-30 11:25:07 -0400

    deleted node modules, changed img import

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from "react";
+import logo from "./images/react-icon-small.png";
 
 export default function Navbar() {
   return (
     <header>
       <nav>
-        <img
-          src="components/images/react-icon-small.png"
-          className="nav--icon"
-        />
+        <img src={logo} className="nav--icon" />
         <h3 className="nav--logo_text">React Facts</h3>
         <h4 className="nav--title">React Course - Project 1</h4>
         <ul className="nav-items">
