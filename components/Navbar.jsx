b7a415a9aed0693527a86e6c5cd0457dbcd9a463
Stacey <stacey.l.joseph@gmail.com>
2024-05-30 11:34:57 -0400

    removed build folder and ul

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,11 +8,6 @@ export default function Navbar() {
         <img src={logo} className="nav--icon" />
         <h3 className="nav--logo_text">React Facts</h3>
         <h4 className="nav--title">React Course - Project 1</h4>
-        <ul className="nav-items">
-          <li>Pricing</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
       </nav>
     </header>
   );
