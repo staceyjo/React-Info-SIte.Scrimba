e4525d4be59ebbada353dd1b19a61eadab52fa93
Stacey <stacey.l.joseph@gmail.com>
2024-05-30 10:19:50 -0400

    final styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="footer">
-      <small>© 2024 Joseph development. All rights reserved.</small>
+      <small>
+        © 2024 Gr8 Scrimba Sprint development. All rights reserved.{" "}
+      </small>
     </footer>
   );
 }
